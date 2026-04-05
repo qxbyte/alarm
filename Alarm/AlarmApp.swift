13de7f56680a2e489e65f942a70b93cbda1df576
Elio Xue <elio_xue@icloud.com>
2026-04-05 16:09:49 +0800

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by 薛强 on 2026/4/5.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alarm
+//
+//  Created by 薛强 on 2026/4/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
